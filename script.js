21a578c40e1ab2d78749e90736bb8ff091dec2e3
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2024-08-25 17:12:17 -0700

    stat counter

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuIcon = document.getElementById("menu-icon");
     const navLinks = document.querySelector(".nav-links");
 
-  
     menuIcon.addEventListener("click", function () {
         navLinks.classList.toggle("active");
     });
 
-  
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) {
             navbar.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
@@ -19,3 +17,24 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+const counters = document.querySelectorAll('.stat-number');
+const speed = 200; 
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
+
